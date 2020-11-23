0137fe1eb7249eb9dd09ad80b249c60adfb39084
deepukchandran <74598901+deepukchandran@users.noreply.github.com>
2020-11-23 10:48:24 +0000

    Update inshackle.sh

--- inshackle.sh
+++ inshackle.sh
@@ -1,7 +1,7 @@
-C #!/bin/bash
-# Inshackle v1.0
-# recoded by: CYBER KALLAN
-# github.com/cyberkallan/inshackle-bot
+#!/bin/bash
+# Inshackle version 1
+# coded by: @nandy121
+# github.com/nandy121/inshackle
 # If you use any part from this code, give me the credits, please, read the License
 
 
@@ -31,7 +31,7 @@ printf " \e[1;31mM  M\e[0m\e[1;77m dP    dP \`88888P\' \e[0m\e[1;93mdP    dP \`8
 printf " \e[1;31mMMMM\e[0m                                                                    \n"
 printf "\n"
 
-printf "      \e[1;31m[\e[0m\e[1;77mv1.0\e[0m\e[1;31m]\e[0m\e[1;77m recoded by cyber kallan (thanks to linuxchoice )\e[0m\n"
+printf "      \e[1;31m[\e[0m\e[1;77mv1.0\e[0m\e[1;31m]\e[0m\e[1;77m github.com/thelinuxchoice/inshackle\e[0m\n"
 }
 
 
